243354f78bd3e6774b08904fc326767eb5d2de62
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-11-30 12:47:43 +0200

    bridge pattern

--- structural_patterns/composite.py
+++ structural_patterns/composite.py
@@ -10,13 +10,13 @@ class Component(object):
 class Child(Component): #Inherits from the abstract class, Component
 	"""Concrete class"""
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, *args, **kwargs) -> None:
 		Component.__init__(self, *args, **kwargs)
 
 		#This is where we store the name of your child item!
-		self.name = args[0]
+		self.name: str = args[0]
 
-	def component_function(self):
+	def component_function(self) -> None:
 		#Print the name of your child item here!
 		print("{}".format(self.name))
 
@@ -27,20 +27,20 @@ class Composite(Component): #Inherits from the abstract class, Component
 		Component.__init__(self, *args, **kwargs)
 
 		#This is where we store the name of the composite object
-		self.name = args[0]
+		self.name: str = args[0]
 
 		#This is where we keep our child items
-		self.children = []
+		self.children: list = []
 
-	def append_child(self, child):
+	def append_child(self, child) -> None:
 		"""Method to add a new child item"""
 		self.children.append(child)
 
-	def remove_child(self, child):
+	def remove_child(self, child) -> None:
 		"""Method to remove a child item"""
 		self.children.remove(child)
 
-	def component_function(self):
+	def component_function(self) -> None:
 
 		#Print the name of the composite object
 		print("{}".format(self.name))
@@ -51,17 +51,17 @@ class Composite(Component): #Inherits from the abstract class, Component
 
 
 if __name__ == '__main__':
-    sub1 = Composite("submenu1") # Build a composite submenu 1
+    sub1: Composite = Composite("submenu1") # Build a composite submenu 1
 
-    sub11 = Child("sub_submenu 11") # Create a new child sub_submenu 11
-    sub12 = Child("sub_submenu 12") #Create a new Child sub_submenu 12
+    sub11: Child = Child("sub_submenu 11") # Create a new child sub_submenu 11
+    sub12: Child = Child("sub_submenu 12") #Create a new Child sub_submenu 12
 
     sub1.append_child(sub11) # Add the sub_submenu 11 to submenu 1
     sub1.append_child(sub12) # Add the sub_submenu 12 to submenu 1
 
-    top = Composite("top_menu") # Build a top-level composite menu
+    top: Composite = Composite("top_menu") # Build a top-level composite menu
 
-    sub2 = Child("submenu2") # Build a submenu 2 that is not a composite
+    sub2: Child = Child("submenu2") # Build a submenu 2 that is not a composite
 
     top.append_child(sub1) # Add the composite submenu 1 to the top-level composite menu
     top.append_child(sub2) # Add the plain submenu 2 to the top-level composite menu
