6f7d8884f69d0aed2206cbd3ede38847907a3614
Ahmed Abdelkarim <a.abdelkarim9696@gmail.com>
2023-11-30 12:50:23 +0200

    complete structrual patterns

--- structural_patterns/bridge.py
+++ structural_patterns/bridge.py
@@ -16,38 +16,38 @@ class Draw(ABC):
 
 class DrawingAPIOne(Draw):
 	"""Implementation-specific abstraction: concrete class one"""
-	def draw_circle(self, x, y, radius):
+	def draw_circle(self, x: float, y: float, radius: float) -> None:
 		print(f"API 1 drawing a circle at ({x}, {y} with radius {radius}!)")
 
 
 class DrawingAPITwo(Draw):
 	"""Implementation-specific abstraction: concrete class two"""
-	def draw_circle(self, x, y, radius):
+	def draw_circle(self, x: float, y: float, radius: float) -> None:
 		print(f"API 2 drawing a circle at ({x}, {y} with radius {radius}!)")
 
 class Circle:
 	"""Implementation-independent abstraction: for example, there could be a rectangle class!"""
 
-	def __init__(self, x, y, radius, drawing_api):
+	def __init__(self, x: float, y: float, radius: float, drawing_api: object) -> None:
 		"""Initialize the necessary attributes"""
-		self._x = x
-		self._y = y
-		self._radius = radius
-		self._drawing_api = drawing_api
+		self._x: float = x
+		self._y: float = y
+		self._radius: float = radius
+		self._drawing_api: DrawingAPIOne or DrawingAPITwo = drawing_api
 
-	def draw(self):
+	def draw(self) -> None:
 		"""Implementation-specific abstraction taken care of by another class: DrawingAPI"""
 		self._drawing_api.draw_circle(self._x, self._y, self._radius)
 
-	def scale(self, percent):
+	def scale(self, percent) -> None:
 		"""Implementation-independent"""
 		self._radius *= percent
 
 
 if __name__ == '__main__':
-    circle1 = Circle(1, 2, 3, DrawingAPIOne()) # Build the first Circle object using API One
+    circle1: Circle = Circle(1, 2, 3, DrawingAPIOne()) # Build the first Circle object using API One
     circle1.draw() # Draw a circle
 
-    circle2 = Circle(2, 3, 4, DrawingAPITwo()) # Build the second Circle object using API Two
+    circle2: Circle = Circle(2, 3, 4, DrawingAPITwo()) # Build the second Circle object using API Two
     circle2.draw() # Draw a circle
 
