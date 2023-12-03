4f7a759971736f45e616a61c38ce3cd8e5685919
Ahmed Abdelkarim <56482735+a-abdelkarim@users.noreply.github.com>
2023-12-03 13:43:26 +0200

    Update observer.py

--- behavioural_patterns/observer.py
+++ behavioural_patterns/observer.py
@@ -8,18 +8,18 @@ class Subject: # Represents what is being 'observed'
 	def __init__(self):
 		self._observers = [] # This where references to all the observers are being kept
 							 # Note that this is a one-to-many relationship: there will be one subject to 
-                             # be observed by multiple _observers
-
+			     # be observed by multiple _observers
+	
 	def attach(self, observer):
 		if observer not in self._observers: # If the observer is not already in the observers list
 			self._observers.append(observer) # append the observer to the list
-
+	
 	def detach(self, observer): # Simply remove the observer
 		try:
 			self._observers.remove(observer)
 		except ValueError:
 			pass
-
+	
 	def notify(self, modifier=None):
 		for observer in self._observers: # For all the observers in the list
 			if modifier != observer: # Don't notify the observer who is actually updating the temperature 
